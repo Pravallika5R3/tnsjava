4687d003acdaf3fb4097d868670bfddbfa8d296a
Pravallika5R3 <pravallikayalam@gmail.com>
2025-09-20 13:18:25 +0530

    next commit

---
+++ teegala/src/com/tnsif/operations/Arithematic.java
@@ -0,0 +1,24 @@
+package com.tnsif.operations;
+
+public class Arithematic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=100;
+		int y=20;
+		char z='d';
+		System.out.println((x + z)); 
+		System.out.println((x - y));
+		System.out.println(x * y); 
+		System.out.println(x / y);
+		System.out.println(x % y);
+		 
+		System.out.println(++y);
+		System.out.println(--y);
+		System.out.println(y++);
+		System.out.println(y--);
+
+
+	}
+
+}
