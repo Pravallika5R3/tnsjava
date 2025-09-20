53c8fccbbc81e400a959cee24d72156f730fd794
Pravallika5R3 <pravallikayalam@gmail.com>
2025-09-20 13:11:09 +0530

    initial commit

---
+++ teegala/src/com/tnsif/javabasic/dataTypes.java
@@ -0,0 +1,33 @@
+package com.tnsif.javabasic;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive datatypes
+				byte num0=127;//can strore whole numbers from -128 to 127 size=8
+				short num1=32767;//-32768 to 32767  16
+				int num3 = 10;//-2,147,483,648 to 2,147,483,647   32
+				long num4=45;//9,223,372,036,854,775,808 to 9,223,372,036,854,775,807    64
+				float num5=67;// 6 to 7 decimal digits   32
+				double num6=78.00;//15 to 16 decimal digits   64
+				boolean a=true ;//or false 1 to 8 bts
+				char b='a';//single character     16
+				//Non-primitive data types - such as String, Arrays and Classes 
+			    
+				System.out.println(num0);
+				System.out.println(num1);
+				//System.out.println(num2);
+				System.out.println(num3);
+				System.out.println(num4);
+				System.out.println(num5);
+				System.out.println(num6);
+				System.out.println(a);
+				System.out.println(b);
+		    
+			
+
+
+	}
+
+}
